9cf8568c023e1cb45c655fb4ceba5a0f222e6626
kluczi <bartekcsgo04@gmail.com>
2025-11-29 21:11:33 +0100

    feat(core): add returns value and returned products value by customer

---
+++ scripts/core/refunds_by_customer.sql
@@ -0,0 +1,23 @@
+with ecommerce_parsed as (
+    select
+        customer_id,
+        quantity,
+        unit_price,
+        description
+    from staging.ecommerce
+)
+select
+    customer_id as customer_id,
+    sum(quantity) as refunds,
+    sum(quantity*unit_price) as losses
+from ecommerce_parsed
+where
+    quantity<0
+    and description = upper(description)
+    and description != lower(description)
+    and description is not null
+    and customer_id is not null
+group by customer_id
+order by 2 ASC;
+
+
