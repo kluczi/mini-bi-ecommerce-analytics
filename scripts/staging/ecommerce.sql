96f526b36988d6edb9bf06d9d8c33dae25e632e0
kluczi <bartekcsgo04@gmail.com>
2025-11-29 18:15:38 +0100

    project setup; created first pipeline and simple query to test

---
+++ scripts/staging/ecommerce.sql
@@ -0,0 +1,14 @@
+select
+    "InvoiceNo"      as invoice_no,
+    "StockCode"      as stock_code,
+    "Description"   as description,
+    "Quantity"::int as quantity,
+    "InvoiceDate"   as invoice_date_raw,
+    "UnitPrice"::double as unit_price,
+    "CustomerID"    as customer_id,
+    "Country"       as country
+from read_csv(
+    'data/ecommerce.csv',
+    header = true,
+    ignore_errors = true
+);
