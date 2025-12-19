f0de8dbca1680930d73135cac3c81e434223680d
kluczi <bartekcsgo04@gmail.com>
2025-12-19 16:47:01 +0100

    Update dim_invoice.sql

--- scripts/intermediate/dim_invoice.sql
+++ scripts/intermediate/dim_invoice.sql
@@ -1,5 +1,5 @@
-select
+select distinct
+    md5(trim(cast(invoice_no as varchar))) as invoice_sk,
     invoice_no,
-    quantity,
-    invoice_ts
+    try_strptime(invoice_date_raw, '%m/%d/%Y %H:%M') as invoice_ts
 from staging.stg_ecommerce;
