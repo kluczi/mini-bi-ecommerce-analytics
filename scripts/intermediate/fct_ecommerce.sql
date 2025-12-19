8a75c3facf11d751af825ec9b3c853ea0f9892c7
kluczi <bartekcsgo04@gmail.com>
2025-12-19 16:47:02 +0100

    Update fct_ecommerce.sql

--- scripts/intermediate/fct_ecommerce.sql
+++ scripts/intermediate/fct_ecommerce.sql
@@ -1,10 +1,11 @@
 select
-    invoice_no,
-    stock_code,
-    description,
-    quantity,
-    try_strptime(invoice_date_raw, '%m/%d/%Y %H:%M') as invoice_ts,
-    unit_price,
-    customer_id,
-    country
-from staging.stg_ecommerce
+    dp.product_sk,
+    di.invoice_sk,
+    dc.customer_sk,
+    stg.quantity,
+    stg.unit_price,
+    stg.quantity*stg.unit_price as order_value
+from staging.stg_ecommerce stg
+JOIN intermediate.dim_customer dc ON stg.customer_id=dc.customer_id
+JOIN intermediate.dim_invoice di ON stg.invoice_no=di.invoice_no
+JOIN intermediate.dim_product dp ON stg.stock_code=dp.stock_code;
