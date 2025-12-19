af6ec37ebd31b5df2ce3d4f76e11fd44425d3cb9
kluczi <bartekcsgo04@gmail.com>
2025-12-19 16:47:02 +0100

    Update dim_product.sql

--- scripts/intermediate/dim_product.sql
+++ scripts/intermediate/dim_product.sql
@@ -1,7 +1,7 @@
-select
+select distinct
+    md5(trim(lower(cast(stock_code as varchar)))) as product_sk,
     stock_code,
-    description,
-    unit_price
+    description
 from staging.stg_ecommerce
 where
     description is not null;
